dad626385c1db5b438b1c827064b09d680bbfeb1
MinimalGhost <eric.kollegger@gmail.com>
2018-06-17 17:44:55 -0400

    repairs and infantry deployment

--- algo_strategy.py
+++ algo_strategy.py
@@ -105,3 +105,54 @@ class AlgoStrategy(gamelib.AlgoCore):
             radius = self.holeradius
         funnel_x, _ = self.funnel_point
         return abs(x - funnel_x) <= radius
+
+    def repair_wall(self, game_map):
+        """ puts towers on the 13th row so that the hole is the only open location """
+        locations = [[x, 13] for x in range(0, game_map.boardSize)]
+
+        # Spawn TB away from the hole, and TE near the hole.
+        funnel_compatible_locations_TB = [loc for loc in locations
+                                          if not self.blocks_funnel(loc[0], self.holeradiusWide)]
+
+        funnel_compatible_locations_TE = [loc for loc in locations
+                                          if (self.blocks_funnel(loc[0], self.holeradiusWide)) and
+                                          (not self.blocks_funnel(loc[0]))]
+
+        game_map.attempt_spawn_multiple("TE", funnel_compatible_locations_TE)
+        game_map.attempt_spawn_multiple("TB", funnel_compatible_locations_TB)
+
+    def deploy_all_split_strategy(self, type, game_map):
+        for i in range(game_map.number_affordable(type)):
+            location = [5, 8] if i < 5 else [13, 0]
+            game_map.spawn_unit(type, location)
+
+    def build_supports(self, game_map):
+        '''
+        Builds support towers leading up to the hole. If the hole is completely blocked it also builds TB towers on a
+        side of the hole so that it can funnel tanks to self destruct near the hole.
+        Support towers are built in a diagonal line leading up the hole based on a few origin points in relation to
+        the hole location.
+        :param game_map:
+        :return:
+        '''
+        if not self.supportStartPoint:
+            for x in reversed(range(0, game_map.boardSize)):
+                if not game_map.is_blocked([x, 13]) and self.blocks_funnel(x):
+                    self.supportStartPoint = [x, 12]
+                    break
+
+        # If path is blocked completely, create TBs so that you can explode tanks in a good spot
+        # Don't need anymore because new self destruct pathing!
+        if not self.pathFound and False:
+            self.funnel_to_self_destruct(self.supportStartPoint, game_map)
+
+        bottom_left_edge = game_map.get_edge_locations('bottom_left')
+        offset_points = [[loc[0], loc[1] + 1] for loc in bottom_left_edge]
+        skip_build_points = bottom_left_edge + offset_points
+
+        # get "origin points" for the TS tails, encoded here as horizontal offsets from self.supportStartPoint
+        # build on origin points until out of metal or get to edge point and edge point +1y
+        x0, y0 = self.supportStartPoint
+        horizontal_offsets = [-3, 1, 2, -4]
+        for origin_point in [(x0 + x_offset, y0) for x_offset in horizontal_offsets]:
+            self.build_offset_supports(origin_point, skip_build_points, game_map)
